8b6c53c753b1691256baf23d649c16ce603e41c1
vigneshjm <vigneshjm96@gmail.com>
2017-02-15 18:09:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article two requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article three requested');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
