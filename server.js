6f5d5d831822d762d9f3afa60d587dde85fba33a
vigneshjm <vigneshjm96@gmail.com>
2017-03-11 20:02:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/article-three', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
   
 });
+var counter=0;
+app.get('/counter', function (req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
