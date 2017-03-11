4cb5fade6deb48b7f432cdc5c30d9a48da74cc77
vigneshjm <vigneshjm96@gmail.com>
2017-03-11 19:25:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,9 @@ element.innerHTML='New Value';
 var img=document.getElementById('dino');
 var marginLeft=0;
 function moveRight(){
- 
  marginLeft=marginLeft+5;
  img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function(){
-    var interval=SetInterval(moveRight,100);
+    var interval=setInterval(moveRight,100);
 };
