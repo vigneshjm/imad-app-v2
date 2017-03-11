432f2723733ccb2c02b0d3a7461e5f40129069bd
vigneshjm <vigneshjm96@gmail.com>
2017-03-11 19:22:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='New Value';
 
 var img=document.getElementById('dino');
 var marginLeft=0;
-function moveright(){
+function moveRight(){
  
  marginLeft=marginLeft+5;
  img.style.marginLeft=marginLeft + 'px';
